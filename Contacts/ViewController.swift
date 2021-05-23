1d379ea5dcf1c3fe572601715ea95ccb7b1ed104
Ekaterina Grigoryeva <ekaterina_grigoryeva97@mail.ru>
2021-05-23 13:44:04 +0700

    настроила navigation

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchControllerDelegate {
+	private let searchController: UISearchController = {
+		let searchController = UISearchController(searchResultsController: nil)
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchBar.placeholder = "Search"
+		searchController.searchBar.backgroundColor = .clear
+		searchController.searchBar.tintColor = .black
+		return searchController
+	}()
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		view.backgroundColor = #colorLiteral(red: 0.8146950603, green: 0.8098530769, blue: 0.8184176087, alpha: 1)
+		
+		searchController.delegate = self
+		
+		navigationItem.title = "Contacts"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(plusPressed))
+		navigationItem.searchController = searchController
 	}
 
+	@objc private func plusPressed() {
+		
+	}
 }
